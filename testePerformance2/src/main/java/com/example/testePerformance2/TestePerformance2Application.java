57997eb8a24ee8ced39c3a8f8f4cf353559b1b27
Uendel Ives de Araujo <uendel.araujo@al.infnet.edu.br>
2025-08-18 12:52:53 -0400

    Added the project created by springInitializeWeb and gitIgnore file

---
+++ testePerformance2/src/main/java/com/example/testePerformance2/TestePerformance2Application.java
@@ -0,0 +1,13 @@
+package com.example.testePerformance2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestePerformance2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestePerformance2Application.class, args);
+	}
+
+}
---
+++ testePerformance2/src/test/java/com/example/testePerformance2/TestePerformance2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testePerformance2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestePerformance2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
