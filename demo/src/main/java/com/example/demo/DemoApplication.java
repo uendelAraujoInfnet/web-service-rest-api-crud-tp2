42760ab4537e0ee46ae3a03923e2ebe10f281a1f
Uendel Ives de Araujo <uendel.araujo@al.infnet.edu.br>
2025-08-18 15:34:44 -0400

    Change file's name

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,13 @@
-package com.example.testePerformance2;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TestePerformance2Application {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TestePerformance2Application.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
